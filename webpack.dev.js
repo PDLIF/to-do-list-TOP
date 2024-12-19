2fbfd0c284c2a52a432c5ea69b450841df53681b
astuarykig <akvsevolod@icloud.com>
2024-12-19 16:51:11 +0500

    feat: implementing webpack with 2 modes (dev and prod)

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // Inject styles into the DOM
+            },
+        ],
+    },
+});
